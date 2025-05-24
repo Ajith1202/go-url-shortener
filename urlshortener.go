10cc39bca1b62a5fa12d2d40c8840616ee8b3b72
Ajith Arakkal <ajith.arakkal@oracle.com>
2025-05-24 17:19:05 +0530

    Replaced in-memory map with Redis implementation

--- urlshortener.go
+++ urlshortener.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
-var urlMap = make(map[string]string)
+var backgroundCtx = context.Background()
+
+func GetRedisClient() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return rdb
+}
 
 func main() {
 
+	redisClient := GetRedisClient()
 	restEngine := gin.Default()
 	restEngine.POST("/urlShorten", func(ctx *gin.Context) {
 		var req struct {
@@ -30,16 +43,25 @@ func main() {
 		}
 
 		shortUrl := getShortenedUrl()
-		urlMap[shortUrl] = req.URL
+
+		err = redisClient.Set(backgroundCtx, shortUrl, req.URL, 0).Err()
+		if err != nil {
+			ctx.JSON(400, gin.H{"Error": "Unexpected error while storing to database"})
+			return
+		}
+
 		ctx.JSON(200, gin.H{"Shortened URL": "http://localhost:8080/" + shortUrl})
 	})
 
 	restEngine.GET("/:code", func(ctx *gin.Context) {
 		code := ctx.Param("code")
 
-		longUrl, exists := urlMap[code]
-		if !exists {
+		longUrl, err := redisClient.Get(backgroundCtx, code).Result()
+		if err == redis.Nil {
 			ctx.JSON(404, gin.H{"Error": "URL Not Found"})
+			return
+		} else if err != nil {
+			ctx.JSON(400, gin.H{"Error": "Unexpected error while fetching from database"})
 			return
 		}
 
